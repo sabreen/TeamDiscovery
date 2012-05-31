b50d86edc2695ace61fd0de1a0321a984cb7801a
AlaaHamoudah <alaakhalil11@gmail.com>
2012-05-31 16:19:27 +0300

    Update view/new  2.php

--- view/new  2.php
+++ view/new  2.php
@@ -3,6 +3,11 @@
   <div>
   <h1>Hello !</h1>
 	<?php echo "haha"?>
+	<?php echo "haha"?>
+	<?php echo "haha"?>
+	<?php echo "haha"?>
+	<?php echo "haha"?>
+	<?php echo "haha"?>
   </div>
   </body>
 </html>
