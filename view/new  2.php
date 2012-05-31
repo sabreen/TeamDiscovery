043222092bd8bce159d1b01a856195518e109583
AlaaHamoudah <alaakhalil11@gmail.com>
2012-05-31 16:23:09 +0200

    Revert "Update view/new  2.php"
    This reverts commit b50d86edc2695ace61fd0de1a0321a984cb7801a.

--- view/new  2.php
+++ view/new  2.php
@@ -3,11 +3,6 @@
   <div>
   <h1>Hello !</h1>
 	<?php echo "haha"?>
-	<?php echo "haha"?>
-	<?php echo "haha"?>
-	<?php echo "haha"?>
-	<?php echo "haha"?>
-	<?php echo "haha"?>
   </div>
   </body>
 </html>
